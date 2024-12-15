42475a4ec3a5a0249d94d3539b9aaefd8ae3c728
damiano <macmacroll@gmail.com>
2024-12-15 18:08:20 +0100

    Updated database schema adding uuid4 as id and not null to all foreign keys

--- database/V1_init.sql
+++ database/V1_init.sql
@@ -1,65 +1,67 @@
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA IF NOT EXISTS public;
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE "user"
 (
-    id    SERIAL PRIMARY KEY,
-    name  TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE
+    id    UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    name  TEXT                                        NOT NULL,
+    email TEXT                                        NOT NULL UNIQUE
 );
 
 CREATE TABLE label
 (
-    id   SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE
+    id   UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    name TEXT                                        NOT NULL UNIQUE
 );
 
 CREATE TABLE course
 (
-    id     SERIAL PRIMARY KEY,
-    stages INTEGER NOT NULL
+    id     UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    stages INTEGER                                     NOT NULL
 );
 
 CREATE TABLE content
 (
-    id           SERIAL PRIMARY KEY,
-    path         TEXT NOT NULL,
-    title        TEXT NOT NULL,
-    mail_content TEXT NOT NULL
+    id           UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    path         TEXT                                        NOT NULL,
+    title        TEXT                                        NOT NULL,
+    mail_content TEXT                                        NOT NULL
 );
 
 CREATE TABLE user_labels
 (
-    id       SERIAL PRIMARY KEY,
-    user_id  INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
-    label_id INTEGER REFERENCES label (id) ON DELETE CASCADE,
-    matching INT2 NOT NULL,
+    id       UUID PRIMARY KEY DEFAULT uuid_generate_v4()   NOT NULL,
+    user_id  UUID REFERENCES "user" (id) ON DELETE CASCADE NOT NULL,
+    label_id UUID REFERENCES label (id) ON DELETE CASCADE  NOT NULL,
+    matching INT2                                          NOT NULL,
     UNIQUE (user_id, label_id)
 );
 
 CREATE TABLE user_courses
 (
-    id        SERIAL PRIMARY KEY,
-    user_id   INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
-    course_id INTEGER REFERENCES course (id) ON DELETE CASCADE,
-    stage     INTEGER NOT NULL,
+    id        UUID PRIMARY KEY DEFAULT uuid_generate_v4()   NOT NULL,
+    user_id   UUID REFERENCES "user" (id) ON DELETE CASCADE NOT NULL,
+    course_id UUID REFERENCES course (id) ON DELETE CASCADE NOT NULL,
+    stage     INTEGER                                       NOT NULL,
     UNIQUE (user_id, course_id)
 );
 
 CREATE TABLE content_labels
 (
-    id         SERIAL PRIMARY KEY,
-    content_id INTEGER REFERENCES content (id) ON DELETE CASCADE,
-    label_id   INTEGER REFERENCES label (id) ON DELETE CASCADE,
-    matching   INT2 NOT NULL,
+    id         UUID PRIMARY KEY DEFAULT uuid_generate_v4()    NOT NULL,
+    content_id UUID REFERENCES content (id) ON DELETE CASCADE NOT NULL,
+    label_id   UUID REFERENCES label (id) ON DELETE CASCADE   NOT NULL,
+    matching   INT2                                           NOT NULL,
     UNIQUE (content_id, label_id)
 );
 
 CREATE TABLE course_contents
 (
-    id         SERIAL PRIMARY KEY,
-    course_id  INTEGER REFERENCES course (id) ON DELETE CASCADE,
-    content_id INTEGER REFERENCES content (id) ON DELETE CASCADE,
-    stage      INTEGER NOT NULL,
+    id         UUID PRIMARY KEY DEFAULT uuid_generate_v4()    NOT NULL,
+    course_id  UUID REFERENCES course (id) ON DELETE CASCADE  NOT NULL,
+    content_id UUID REFERENCES content (id) ON DELETE CASCADE NOT NULL,
+    stage      INTEGER                                        NOT NULL,
     UNIQUE (course_id, content_id)
 );
