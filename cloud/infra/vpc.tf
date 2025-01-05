20d6555d393d1c32fc21b8f43e1ac3efff7795be
damiano <macmacroll@gmail.com>
2025-01-05 14:44:45 +0100

    Removed vpc endpoint

--- cloud/infra/vpc.tf
+++ cloud/infra/vpc.tf
@@ -50,36 +50,6 @@ resource "aws_route_table_association" "zdrovi_route_table_association" {
   route_table_id = aws_route_table.zdrovi_public_route_table.id
 }
 
-# VPC Endpoint for EC2 Instance Connect
-resource "aws_vpc_endpoint" "zdrovi_server_vpc_endpoint" {
-  vpc_id             = aws_vpc.zdrovi_vpc.id
-  service_name       = "com.amazonaws.${var.region}.ec2-instance-connect"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = [aws_subnet.zdrovi_public_subnet.id]
-
-  security_group_ids = [aws_security_group.zdrovi_server_vpc_endpoint_security_group.id]
-
-  private_dns_enabled = true
-}
-
-# Security Group for VPC Endpoint
-resource "aws_security_group" "zdrovi_server_vpc_endpoint_security_group" {
-  name        = "zdrovi_server_vpc_endpoint_security_group"
-  description = "Security group for VPC Endpoint"
-  vpc_id      = aws_vpc.zdrovi_vpc.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
-  }
-
-  tags = {
-    Name = "zdrovi_server_vpc_endpoint_security_group"
-  }
-}
-
 # Security Group for EC2
 resource "aws_security_group" "zdrovi_server_security_group" {
   name        = "zdrovi_server_security_group"
