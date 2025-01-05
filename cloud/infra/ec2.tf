8e3e666b2b5d43c7120d3aab661a5ce0a475b88f
damiano <macmacroll@gmail.com>
2025-01-05 15:22:16 +0100

    Created ssh key for zdrovi server

--- cloud/infra/ec2.tf
+++ cloud/infra/ec2.tf
@@ -56,6 +56,25 @@ resource "aws_iam_role_policy" "zdrovi_server_instance_connect_policy" {
   })
 }
 
+# Create key pair
+resource "aws_key_pair" "zdrovi_server_key" {
+  key_name   = "zdrovi_server_key"
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+
+# Generate SSH key
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# Store private key in SSM Parameter Store
+resource "aws_ssm_parameter" "zdrovi_server_ssh_private_key" {
+  name  = "/ec2/zdrovi_server_ssh_private_key"
+  type  = "SecureString"
+  value = tls_private_key.ssh.private_key_pem
+}
+
 # Create instance profile
 resource "aws_iam_instance_profile" "zdrovi_server_ec2_profile" {
   name = "zdrovi_server_ec2_profile"
@@ -68,6 +87,7 @@ resource "aws_instance" "zdrovi_server" {
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.zdrovi_public_subnet.id
   vpc_security_group_ids = [aws_security_group.zdrovi_server_security_group.id]
+  key_name                    = aws_key_pair.zdrovi_server_key.key_name
   associate_public_ip_address = true
 
   metadata_options {
@@ -82,7 +102,7 @@ resource "aws_instance" "zdrovi_server" {
 
   root_block_device {
     volume_size           = 8
-    volume_type          = "gp2"
+    volume_type           = "gp2"
     delete_on_termination = true
   }
 }
